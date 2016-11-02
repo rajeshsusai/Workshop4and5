35d545e050b3627e897946b3fe9705abaeed8ca9
Rajesh Susai <rajeshsusai14@gmail.com>
2016-11-02 00:40:49 -0400

    fb5

--- app/components/feed.js
+++ app/components/feed.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import FeedItem from './feeditem';
 import StatusUpdateEntry from './statusupdateentry';
+import {getFeedData} from '../server';
 
 export default class Feed extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contents: []
+    };
+  }
   render() {
-    return ( <div>
+    return (
+      <div>
         <StatusUpdateEntry />
-        <FeedItem />
+        {this.state.contents.map((feedItem) => {
+          return (
+            <FeedItem key={feedItem._id} data={feedItem} />
+          );
+        })}
       </div>
     )
-  } 
+  }
+  componentDidMount() {
+    getFeedData(this.props.user, (feedData) => {
+      this.setState(feedData);
+    });
+  }
 }
