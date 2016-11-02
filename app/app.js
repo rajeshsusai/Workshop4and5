a07c336a722d1c3ff3c4a31eaffb3d1f5e562cfa
Rajesh Susai <rajeshsusai14@gmail.com>
2016-11-02 15:58:07 -0400

    fb7

--- app/app.js
+++ app/app.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Feed from './components/feed';
+import { IndexRoute, Router, Route, browserHistory } from 'react-router';
 
-ReactDOM.render(
-  <Feed user={4} />,
-  document.getElementById('fb-feed')
-);
+class ProfilePage extends React.Component {
+  render() {
+    return (
+      <p>This is the profile page for a user with ID {this.props.params.id}.
+      </p>
+    );
+  }
+}
+class FeedPage extends React.Component {
+  render() {
+    return <Feed user={4} />;
+  }
+}
+class App extends React.Component {
+  render() {
+    return (
+      <div>{this.props.children}</div>
+    )
+  }
+}
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      {/* Show the Feed at/ */}
+      <IndexRoute component={FeedPage} />
+      <Route path="profile/:id" component={ProfilePage} />
+    </Route>
+  </Router>
+),document.getElementById('fb-feed'));
